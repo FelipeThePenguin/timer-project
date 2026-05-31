d438719cac33ba1fb6d231d6d10ee7ec3ac9ff17
FelipeThePenguin <chrisrockhateseveryone@gmail.com>
2026-05-31 13:34:41 +0800

    Made a server for the web service to run

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 import express from 'express';
 
 const app = express();
-
-app.set('view engine', 'ejs');
-
-app.get("/", (req, res) => {
-  res.sendFile("dist/index.html");
-});
+app.use(express.static('dist'));
 
 app.listen(3000, () => {
   console.log("The server is running");
